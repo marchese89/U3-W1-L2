532ee9bdb0fada4603317cbafda437c8bae2f823
marchese89 <marchese89@hotmail.com>
2023-10-24 14:33:33 +0200

    fine primi passi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
+import AllTheBooks from "./components/AllTheBooks";
+import books from "./books/horror.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyNav />
+      <Welcome />
+      <AllTheBooks genre={books} />
+      <MyFooter />
+    </>
   );
 }
 
